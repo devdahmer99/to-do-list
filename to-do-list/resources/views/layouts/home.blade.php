bb45e309d3c56e62222ccafb5f2f7c9ccb9a74e1
Eduardo Dahmer <eduardodahmer99@gmail.com>
2023-05-20 22:55:27 -0300

    - Finalizaçao da tela home do sistema.

---
+++ to-do-list/resources/views/layouts/home.blade.php
@@ -0,0 +1,78 @@
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=0.1">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>TodoApp</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="/assets/css/style.css"/>
+    </head>
+    <body>
+      <div class="container">
+          <div class="sidebar">
+            <img src="/assets/images/logo.png"/>
+          </div>
+          <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">
+                  Criar Tarefa
+                </a>
+            </nav>
+            <main>
+                <section class="graph">
+                 <div class="graph_header">
+                    <h2>Progresso do Dia</h2>
+                    <div class="graph_header-line"></div>
+                    <div class="graph_header-date">
+                      <img src="/assets/images/icon-prev.png">
+                        20 de Dez
+                      <img src="/assets/images/icon-next.png">
+                    </div>
+                </div>
+                <div class="graph_header-subtitle">
+                    Tarefas: <b>3/6</b>
+                </div>
+                <div class="graph-placeholder">
+                </div>
+                <div class="task_left_footer">
+                  <img src="/assets/images/icon-info.png"/>
+                    Restam 3 tarefas para serem realizadas
+                </div>
+                
+                </section>
+                <section class="list">
+                  <div class="list_header">
+                    <select class="list_header-select">
+                        <option value="1">
+                          Todas as Tarefas
+                        </option>
+                    </select>
+                  </div>
+                  <div class="task_list">
+                    <div class="task">
+                        <div class="title">
+                          <input type="checkbox"/>
+                          <div class="task_title">Titulo da Tarefa</div>
+                        </div>
+                        <div class="priority">
+                          <div class="sphere"></div>
+                          <h6>Prioridade</h6>
+                        </div>
+                        <div class="actions">
+                          <a href="#">
+                            <img src="/assets/images/icon-edit.png">
+                          </a>  
+                          <a href="#">
+                            <img src="/assets/images/icon-delete.png">
+                          </a>
+                        </div>
+                    </div>
+                  </div>
+                </section>
+            </main>
+          </div>
+      </div>
+    </body>
+</html>
